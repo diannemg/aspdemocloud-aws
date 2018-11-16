488fc32958efd7162797a596f3d6b69c9df98adc
root <diannemg@me.com>
2018-11-16 20:32:23 +0000

    attach-db fix

--- virtdbnx_db/attach-db.sh
+++ virtdbnx_db/attach-db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#wait for the SQL Server to come up
+#wait for the SQL Server to come up and serve
 sleep 10
 #run TSQL attach.sql
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P My_P@ssw0rd! -d master -Q "USE [master];CREATE DATABASE [virtdbnxContext] ON (FILENAME = '/var/opt/mssql/sqldata/udb/virtdbnxContext.mdf'),(FILENAME = '/var/opt/mssql/sqldata/udb/virtdbnxContext_log.ldf') for ATTACH;"
